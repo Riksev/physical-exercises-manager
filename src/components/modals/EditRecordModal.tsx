235096f89fbbadfb485a77e02df768c67665008d
Riksev <teraria1128@gmail.com>
2025-06-10 00:19:27 +0300

    added workouts and records management. Added compatibility with old data.

--- src/components/modals/EditRecordModal.tsx
+++ src/components/modals/EditRecordModal.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import type { IEditRecordModalProps, IRecord } from '../../interfaces'
+import type { IEditRecordModalProps } from '../../interfaces'
 
 const EditRecordModal = ({
    setIsEditRecordModalOpen,
